0743fc5b96b78aeae6ddf82c3345df4547ec57e4
ruzny <roony0782045336@gmail.com>
2023-05-19 20:48:28 +0530

    boiler plate setup for the routes & controllers

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res, next) => res.send('Server is up and running :)'));
+
+app.use(notFound);
+app.use(errorHandler)
+
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
