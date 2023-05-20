888b0724171c69f2ee2fc3118b35d50cb7d9b003
ruzny <roony0782045336@gmail.com>
2023-05-20 17:14:16 +0530

    added login & logout func

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,21 @@ import generateToken from "../utils/generateToken.js";
 // route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res, next) => {
-  res.json({ message: "Auth User" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && user.matchPassword(password)) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 // @desc    Register a new user
@@ -27,9 +41,9 @@ const registerUser = asyncHandler(async (req, res, next) => {
   if (user) {
     generateToken(res, user._id);
     res.status(200).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email
+      _id: user._id,
+      name: user.name,
+      email: user.email,
     });
   } else {
     res.status(400);
@@ -41,7 +55,12 @@ const registerUser = asyncHandler(async (req, res, next) => {
 // route    POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res, next) => {
-  res.json({ message: "Logout User" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.json({ message: "User logged out" });
 });
 
 // @desc    Get user profile
